bda1ab1660f5214c0e6ac1cf57ac4599f5be8c08
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-09-24 23:30:30 +0000

    Import Mutation Mapping

--- src/main/java/org/fzl/bankaccountservice/web/BankAccountGraphQLController.java
+++ src/main/java/org/fzl/bankaccountservice/web/BankAccountGraphQLController.java
@@ -4,6 +4,7 @@ import org.fzl.bankaccountservice.entities.BankAccount;
 import org.fzl.bankaccountservice.repositories.BankAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
