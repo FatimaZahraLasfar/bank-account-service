0c1ba5bc6443c569d91bc57285487f40c85907fc
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-09-24 19:06:34 +0000

    add AccountProjection

---
+++ src/main/java/org/fzl/bankaccountservice/entities/AccountProjection.java
@@ -0,0 +1,11 @@
+package org.fzl.bankaccountservice.entities;
+
+import org.fzl.bankaccountservice.enums.AccountType;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(types = BankAccount.class, name="p1")
+public interface AccountProjection {
+    public String getId();
+    public AccountType getType();
+    public Double getBalance();
+}
