542f5f036b4221fafc200f9fc281eb2ead13e764
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-09-24 23:15:40 +0000

    define bankAccountById method

--- src/main/java/org/fzl/bankaccountservice/web/BankAccountGraphQLController.java
+++ src/main/java/org/fzl/bankaccountservice/web/BankAccountGraphQLController.java
@@ -3,6 +3,7 @@ package org.fzl.bankaccountservice.web;
 import org.fzl.bankaccountservice.entities.BankAccount;
 import org.fzl.bankaccountservice.repositories.BankAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -16,5 +17,11 @@ public class BankAccountGraphQLController {
     @QueryMapping
     public List<BankAccount> accountsList (){
         return bankAccountRepository.findAll();
+    }
+
+    @QueryMapping
+    public BankAccount bankAccountById (@Argument String id){
+        return bankAccountRepository.findById( id)
+                .orElseThrow(()-> new RuntimeException(String.format("Account %s not found",id)));
     }
 }
