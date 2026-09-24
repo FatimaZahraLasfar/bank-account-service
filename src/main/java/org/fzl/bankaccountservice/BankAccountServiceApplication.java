165dabe8563bed4082749dd4ae8602d28974f6a1
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-09-24 17:03:27 +0000

    Creating some example of accounts to test with

---
+++ src/main/java/org/fzl/bankaccountservice/BankAccountServiceApplication.java
@@ -0,0 +1,37 @@
+package org.fzl.bankaccountservice;
+
+import org.fzl.bankaccountservice.entities.BankAccount;
+import org.fzl.bankaccountservice.enums.AccountType;
+import org.fzl.bankaccountservice.repositories.BankAccountRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.UUID;
+
+@SpringBootApplication
+public class BankAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(BankAccountRepository bankAccountRepository){
+        return args -> {
+            for (int i=0; i<10; i++){
+                BankAccount bankAccount = BankAccount.builder()
+                        .id(UUID.randomUUID().toString())
+                        .type(Math.random()>0.5? AccountType.CURRENT_ACCOUNT:AccountType.Saving_ACCOUNT)
+                        .balance(10000+Math.random()*90000)
+                        .createdAt(new Date())
+                        .currency("MAD")
+                        .build();
+                bankAccountRepository.save(bankAccount);
+            }
+        };
+    }
+
+}
