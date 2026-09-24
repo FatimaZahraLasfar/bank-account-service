6149a81a038cc4331efbd318f07978aa1af1ed6f
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-09-24 19:06:42 +0000

    add BankAccountRequestDTO and BankAccountResponseDTO

---
+++ src/main/java/org/fzl/bankaccountservice/dto/BankAccountResponseDTO.java
@@ -0,0 +1,18 @@
+package org.fzl.bankaccountservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.fzl.bankaccountservice.enums.AccountType;
+
+import java.util.Date;
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class BankAccountResponseDTO {
+    private String id;
+    private Date createdAt;
+    private Double balance;
+    private String currency;
+    private AccountType type;
+
+}
