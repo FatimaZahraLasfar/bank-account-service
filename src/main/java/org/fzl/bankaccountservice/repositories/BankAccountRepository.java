b1d94e949be6d32d113b084409459f16faf17c0d
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-09-24 19:08:07 +0000

    Add BankAccountRepository

--- src/main/java/org/fzl/bankaccountservice/repositories/BankAccountRepository.java
+++ src/main/java/org/fzl/bankaccountservice/repositories/BankAccountRepository.java
@@ -1,9 +1,18 @@
 package org.fzl.bankaccountservice.repositories;
 
 import org.fzl.bankaccountservice.entities.BankAccount;
+import org.fzl.bankaccountservice.enums.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BankAccountRepository extends JpaRepository<BankAccount , String> {
+
+    @RestResource(path = "/byType")
+    List<BankAccount> findByType(@Param("t") AccountType type);
 }
